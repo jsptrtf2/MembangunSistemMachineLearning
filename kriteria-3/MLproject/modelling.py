9c53f461496b27fd05f6f594c223fe0ec07a6991
jsptrtf2 <jolehaksimpang@gmail.com>
2026-06-10 06:16:32 +0700

    final ml pipeline

--- kriteria-3/MLproject/modelling.py
+++ kriteria-3/MLproject/modelling.py
@@ -1,7 +1,6 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
@@ -17,20 +16,19 @@ y_train = train_df["target"]
 X_test = test_df.drop(columns=["target"])
 y_test = test_df["target"]
 
+# SET EXPERIMENT 
 mlflow.set_experiment("insurance_prediction")
 
-with mlflow.start_run():
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
+# tidak pakai start_run()
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
+y_pred = model.predict(X_test)
 
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-    # log metrics
-    mlflow.log_metric("mse", mse)
-    mlflow.log_metric("r2", r2)
+mlflow.log_metric("mse", mse)
+mlflow.log_metric("r2", r2)
 
-    # save model (INI YANG PENTING)
-    mlflow.sklearn.log_model(model, "model")
+mlflow.sklearn.log_model(model, "model")
