a9d12e9d547f7c6fc734a2a6df34420e2781e2ad
jsptrtf2 <jolehaksimpang@gmail.com>
2026-06-10 05:58:28 +0700

    final ml pipeline

--- kriteria-3/MLproject/modelling.py
+++ kriteria-3/MLproject/modelling.py
@@ -6,37 +6,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
-train_path =  "insurance_preprocessing/insurance_train_preprocessed.csv"
+train_path = "insurance_preprocessing/insurance_train_preprocessed.csv"
 test_path = "insurance_preprocessing/insurance_test_preprocessed.csv"
 
 train_df = pd.read_csv(train_path)
-test_df =  pd.read_csv(test_path)
+test_df = pd.read_csv(test_path)
 
 X_train = train_df.drop(columns=["target"])
 y_train = train_df["target"]
 X_test = test_df.drop(columns=["target"])
 y_test = test_df["target"]
 
-# mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_experiment("insurance_prediction")
 
-# mlflow.set_experiment("Insurance_Cost_Prediction")
-
-with mlflow.start_run(run_name="manual_run"):
-    mlflow.sklearn.autolog()
-
+with mlflow.start_run():
     model = RandomForestRegressor()
-    model.fit(X_train,y_train)
-    joblib.dump(model, "model.pkl")
-    mlflow.log_artifact("model.pkl")
+    model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
+
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model"
-    )
+    # log metrics
+    mlflow.log_metric("mse", mse)
+    mlflow.log_metric("r2", r2)
 
-    print(f"Mean Squared Error: {mse}")
-    print(f"R^2 Score: {r2}")
+    # save model (INI YANG PENTING)
+    mlflow.sklearn.log_model(model, "model")
